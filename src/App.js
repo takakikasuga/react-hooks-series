1dc6a44b751bd34547ce841a10a6674419298f48
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-06-10 08:35:16 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,89 +1,108 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 
 function App() {
   return (
     <>
-      <UseMemo></UseMemo>
-      {/* <UseMemoVer2></UseMemoVer2> */}
+      <Parent></Parent>
+      {/* <UseMemoParent></UseMemoParent> */}
+      {/* <ParentFunc></ParentFunc> */}
+      {/* <ParentFuncCallback></ParentFuncCallback> */}
     </>
   );
 }
 
-// 09 - useMemoを利用しない場合、コンポーネントを再レンダーする度に不要な再計算が発生する
-const UseMemo = () => {
+// Parent コンポーネントが再レンダーする度に再レンダーされる
+const Child = ({ count }) => {
+  console.log("子要素がレンダリングされます。");
+  return <p>子要素: {count}</p>;
+}
+
+const Parent = () => {
+  console.log("親要素がレンダリングされます。");
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  // 引数の数値を２倍にして返す。
-  // 不要なループを実行しているため計算にかなりの時間がかかる。
-  const double = count => {
-    console.log('実行されています。')
-    let i = 0;
-    while (i < 1000000000) i++;
-    return count * 2;
-  };
-
-  // count2 を２倍にした値
-  // double(count2) はコンポーネントが再レンダーされる度に実行されるため、
-  // count1 を更新してコンポーネントが再レンダーされた時にも実行されてしまう。
-  // そのため、count1 を更新してコンポーネントを再レンダーする時も時間がかかる。
-  // count1 を更新しても doubledCount の値は変わらないため、count1 を更新した時に
-  // double(count2) を実行する意味がない。したがって、不要な再計算が発生している状態である。
-  // count1 が更新されてコンポーネントが再レンダーされた時は double(count2) が実行されないようにしたい。
-  const doubledCount = double(count2);
-
   return (
     <>
-      <h2>Increment count1</h2>
-      <p>Counter: {count1}</p>
-      <button onClick={() => setCount1(count1 + 1)}>Increment count1</button>
-
-      <h2>Increment count2</h2>
-      <p>
-        Counter: {count2}, {doubledCount}
-      </p>
-      <button onClick={() => setCount2(count2 + 1)}>Increment count2</button>
+      <button onClick={() => setCount1(count1 + 1)}>親要素（count1）：インクリメント</button>
+      <button onClick={() => setCount2(count2 + 1)}>子要素（count2）：インクリメント</button>
+      <p>親要素: {count1}</p>
+      <Child count={count2} />
     </>
   );
 }
 
-// 10 - useMemoで不要な再計算をスキップする
-const UseMemoVer2 = () => {
+
+// UseMemoParent コンポーネントが再レンダーに依存しない。
+const UseMemoChild = React.memo(({ count }) => {
+  console.log("子要素がレンダリングされます。");
+  return <p>子要素: {count}</p>;
+})
+
+const UseMemoParent = () => {
+  console.log("親要素がレンダリングされます。");
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(0);
 
-  // 引数の数値を２倍にして返す。
-  // 不要なループを実行しているため計算にかなりの時間がかかる。
-  const double = count => {
-    console.log('実行されています。')
-    let i = 0;
-    while (i < 1000000000) i++;
-    return count * 2;
-  };
-
-  // count2 を２倍にした値をメモ化する。
-  // 第２引数に count2 を渡しているため、count2 が更新された時だけ値が再計算される。
-  // count1 が更新され、コンポーネントが再レンダーされた時はメモ化した値を利用するため再計算されない。
-  const doubledCount = useMemo(() => {
-    console.log('doubledCount')
-    return double(count2)
-  }, [count2]);
-
   return (
     <>
-      <h2>Increment(fast)</h2>
-      <p>Counter: {count1}</p>
-      <button onClick={() => setCount1(count1 + 1)}>Increment(fast)</button>
-
-      <h2>Increment(slow)</h2>
-      <p>
-        Counter: {count2}, {doubledCount}
-      </p>
-      <button onClick={() => setCount2(count2 + 1)}>Increment(slow)</button>
+      <button onClick={() => setCount1(count1 + 1)}>親要素（count1）：インクリメント</button>
+      <button onClick={() => setCount2(count2 + 1)}>子要素（count2）：インクリメント</button>
+      <p>親要素: {count1}</p>
+      <UseMemoChild count={count2} />
     </>
   );
 }
+
+const ChildFunc = React.memo(({ handleClick }) => {
+  console.log("子要素がレンダリングされます。");
+  return <button onClick={handleClick}>Child</button>;
+});
+const ParentFunc = () => {
+  console.log("親要素がレンダリングされます。");
+
+  const [count, setCount] = useState(0);
+  // 関数はコンポーネントが再レンダーされる度に再生成されるため、
+  // 関数の内容が同じでも、新しい handleClick と前回の handleClick は
+  // 異なるオブジェクトなので、等価ではない。
+  // そのため、コンポーネントが再レンダーされる。
+  const handleClick = () => {
+    console.log('click');
+  };
+
+  return (
+    <>
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>親要素（count1）：インクリメント</button>
+      <ChildFunc handleClick={handleClick} />
+    </>
+  );
+}
+
+const ChildFuncCallback = React.memo(({ handleClick }) => {
+  console.log("子要素がレンダリングされます。");
+  return <button onClick={handleClick}>Child</button>;
+});
+const ParentFuncCallback = () => {
+  console.log("親要素がレンダリングされます。");
+
+  const [count, setCount] = useState(0);
+  // 関数をメモ化すれば、新しい handleClick と前回の handleClick は
+  // 等価になる。そのため、Child コンポーネントは再レンダーされない。
+  const handleClick = useCallback(() => {
+    console.log('click');
+  }, [])
+
+  return (
+    <>
+      <p>Counter: {count}</p>
+      <button onClick={() => setCount(count + 1)}>親要素（count1）：インクリメント</button>
+      <ChildFuncCallback handleClick={handleClick} />
+    </>
+  );
+}
+
 
 export default App;
